912bf1e4ef528fceec5adb574be77257e23632d0
Irtazafar <124657693+Irtazafar@users.noreply.github.com>
2023-03-09 19:44:50 +0500

    Intial_Commit
    Ruby Practice of all the concepts.

---
+++ 04-Range Function.rb
@@ -0,0 +1,7 @@
+#These are the four ways to check if something lies in the range 
+
+a.range?(b,c)
+return a.range?(b,c)
+a.range? b,c
+return a.range? b,c
+
---
+++ 06-Array Intialzation.rb
@@ -0,0 +1,9 @@
+# Initialize 3 variables here as explained in the problem statement
+#array=[]
+array = Array.new 
+
+#array_1 =[nil]
+array_1=Array.new(1)
+
+#array_2 = [10,10]
+array_2 =Array.new(2,10)
